f62c34ec35bb7e18f26ced78ba8aba18760c866d
Noa Lehrer <noa.lehrer@gmail.com>
2021-05-28 11:32:47 -1000

    Initial Commit

---
+++ Project_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project_4
+//
+//  Created by Noa Lehrer on 5/28/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
